7464efffa24dbbc881cf29ce14c35a0f013f32ff
MorganDourdoigne <dourdoigne.morgan@gmail.com>
2023-11-03 09:29:09 +0100

    form ok

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -70,28 +70,28 @@ export default function ContactForm() {
     <>
       <Form noValidate validated={isValidated} onSubmit={handleSubmit}>
         <Form.Group className="mx-auto mb-3 form-group" controlId="name">
-          <Form.Label>Name</Form.Label>
+          <Form.Label>Nom</Form.Label>
           <Form.Control required type="text" placeholder="Your name" />
           <Form.Control.Feedback type="invalid">
-            <h5>Name must be at least one character.</h5>
+            <h5>Le nom doit avoir plus d'un caractère.</h5>
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mx-auto mb-3 form-group" controlId="email">
-          <Form.Label>Email address</Form.Label>
+          <Form.Label>Adresse E-mail</Form.Label>
           <Form.Control
             required
             pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
             placeholder="someone@something.com"
           />
           <Form.Control.Feedback type="invalid">
-            <h5>Please enter a valid email.</h5>
+            <h5>Veuillez entre une adresse e-mail valide.</h5>
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mx-auto mb-3 form-group" controlId="message">
           <Form.Label>Message</Form.Label>
           <Form.Control required as="textarea" placeholder="Your message..." />
           <Form.Control.Feedback type="invalid">
-            <h5>Please provide a valid message.</h5>
+            <h5>Veuillez fournir un message valide.</h5>
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mx-auto text-center form-group">
@@ -103,7 +103,7 @@ export default function ContactForm() {
               disabled={isProcessing}
               className="my-4"
             >
-              Submit{" "}
+              Envoyer{" "}
               {isProcessing && (
                 <Spinner animation="border" variant="success" size="sm" />
               )}
@@ -116,7 +116,7 @@ export default function ContactForm() {
             onClose={() => setSuccess(false)}
             dismissible
           >
-            <Alert.Heading>Success! I will contact you soon.</Alert.Heading>
+            <Alert.Heading>Félicitations ! Votre message a bien été envoyé. Je reviendrais vers vous rapidement.</Alert.Heading>
           </Alert>
           <Alert
             show={danger}
